15ec25c9ecdf2501e8c3957f8fadabdfac1d4439
Jacob Robertson <1941586@cll.qc.ca>
2020-10-19 17:06:06 -0400

    fini

--- ExamenQuestion12/ExamenQuestion12/frmExamenQuestion12.Designer.cs
+++ ExamenQuestion12/ExamenQuestion12/frmExamenQuestion12.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.txtRandom = new System.Windows.Forms.TextBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(304, 385);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(193, 29);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Image random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // txtRandom
+            // 
+            this.txtRandom.Location = new System.Drawing.Point(445, 116);
+            this.txtRandom.Name = "txtRandom";
+            this.txtRandom.Size = new System.Drawing.Size(125, 27);
+            this.txtRandom.TabIndex = 1;
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbImage.Location = new System.Drawing.Point(32, 31);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(300, 300);
+            this.pbImage.TabIndex = 2;
+            this.pbImage.TabStop = false;
+            // 
+            // frmExamenQuestion12
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.txtRandom);
+            this.Controls.Add(this.btnRandom);
+            this.Name = "frmExamenQuestion12";
+            this.Text = "Examen Question 12 - Jacob Robertson";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.TextBox txtRandom;
+        private System.Windows.Forms.PictureBox pbImage;
     }
 }
 
--- ExamenQuestion12/ExamenQuestion12/frmExamenQuestion12.cs
+++ ExamenQuestion12/ExamenQuestion12/frmExamenQuestion12.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Timers;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,10 +14,53 @@ namespace ExamenQuestion12
 {
     public partial class frmExamenQuestion12 : Form
     {
+        Thread Thread1;
+        //System.Timers.Timer Timer1 = new System.Timers.Timer();
         public frmExamenQuestion12()
         {
             InitializeComponent();
+
         }
 
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Thread1 = new Thread(new ThreadStart(ThreadLoop1));
+            Thread1.Start();
+        }
+        public void ThreadLoop1()
+        {
+            Random R = new Random();
+            int random;
+            int compteur = 0;
+            while (Thread1.IsAlive)
+            {
+                random = R.Next(1, 5);
+                compteur++;
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    if (random == 1)
+                    {
+                        pbImage.Load(@"..\..\..\images\1.jpeg");
+                    }
+                    else if (random == 2)
+                    {
+                        pbImage.Load(@"..\..\..\images\2.jpeg");
+                    }
+                    else if (random == 3)
+                    {
+                        pbImage.Load(@"..\..\..\images\3.jpeg");
+                    }
+                    else
+                    {
+                        pbImage.Load(@"..\..\..\images\4.jpeg");
+                    }
+                    txtRandom.Text = random.ToString() + "      (" + compteur.ToString() + ")";
+                    Application.DoEvents();
+                });
+                Thread.Sleep(1000);
+            }
+        }
+
+            
     }
 }
